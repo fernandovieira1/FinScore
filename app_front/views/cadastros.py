5520a8997df34350e004effcac72e3818383c0de
Fernando Vieira <fernandovieira1@outlook.com>
2025-10-28 18:29:33 -0300

    [v. 0.8.33h (ia), de 28/10/2025]

--- app_front/views/cadastros.py
+++ app_front/views/cadastros.py
@@ -4,10 +4,8 @@ def render():
     st.markdown("<h3 style='text-align: left;'>📟 Cadastros</h3>", unsafe_allow_html=True)
     st.markdown(
         """
-        Usuários, Empresas e outros -- a definir.
+        Cadastros de usuários (analistas e gestores) e das empresas clientes.
         
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-        Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-        Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+        FASE 2 do projeto (backend).
         """,
     )
